5510f65bc57ca037a7f09b375d9956a6285c333a
Nick Woodward <nickwoodward5@gmail.com>
2018-07-11 21:39:41 -0700

    Added twitter functionality, added spotify functionality

--- liri.js
+++ liri.js
@@ -7,16 +7,44 @@ var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var inquirer = require('inquirer');
-inquirer.prompt([ 
+inquirer.prompt([
     {
-    type: "checkbox",
-    message: "Choose an action",
-    choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
-    name: "action"
+        type: "list",
+        message: "Choose an action",
+        choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
+        name: "action"
     },
 ]).then(answers => {
-    console.log(answers.action);
-    if(answers.action === "my-tweets"){
-        console.log("true");
-    }
+    if (answers.action === "my-tweets") {
+        var params = { screen_name: 'Nickwoodwardcbc' };
+        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+            if (!error) {
+                var tweetFrom = "\nTweets from: @" + tweets[0].user.screen_name;
+                console.log(tweetFrom);
+                for (var i = 0; i < 20; i++) {
+                    console.log("\n Text of tweet: " + tweets[i].text + "\nOn: " + tweets[i].created_at);
+                }
+            }
+        });
+    } else if (answers.action === "spotify-this-song") {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Choose a song",
+                name: "song",
+            },
+        ]).then(answers => {
+            console.log(answers.song);
+            
+            spotify.search({ type: 'track', query: answers.song }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                } else if (!err) {
+                    //console.log(JSON.stringify(data.tracks.items[0].name, null, 2));
+                    var info = data.tracks.items[0]
+                    console.log("\nAlbum: " + info.album.name + "\nArtist: " + info.artists[0].name + "\nSong Name: " + info.name + "\nPreview Link: " + info.preview_url);
+                };
+            })
+        })
+    };
 });
